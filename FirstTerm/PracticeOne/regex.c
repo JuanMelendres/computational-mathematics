33b636cb6bb5c7cf40dd7f31fa54b0a01f4ac11f
Juan Melendres <juan.mele97@gmail.com>
2019-10-15 13:55:48 -0500

    Add regex.c Library
    Signed-off-by: Juan Melendres <juan.mele97@gmail.com>

---
+++ FirstTerm/PracticeOne/regex.c
@@ -0,0 +1,7 @@
+int match(char *regexp, char *text);
+
+int matchhere(char *regexp, char *text);
+
+int matchstar(int c, char *regexp, char *text);
+
+int matchplus(int c, char *regexp, char *text);
